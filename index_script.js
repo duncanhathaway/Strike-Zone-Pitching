0d9e6f9f8fcdd9d25338886e78f83983a92c1321
duncanhathaway <duncan.hathaway@outlook.com>
2025-09-09 22:19:10 -0700

    Update 0.2

---
+++ index_script.js
@@ -0,0 +1,5 @@
+window.addEventListener('DOMContentLoaded', function () {
+    setTimeout(function () {
+        document.getElementById('loading-overlay').style.display = 'none';
+    }, 5000);
+});
